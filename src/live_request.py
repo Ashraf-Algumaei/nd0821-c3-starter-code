9aa91836da829ca77244de5fadd46f9c41c697b8
Ashraf Al Gumaei <sharafah@vcu.edu>
2022-05-30 13:49:33 -0400

    Added live request script

--- src/live_request.py
+++ src/live_request.py
@@ -3,7 +3,24 @@ Script used to send a sample request to the API and prints the response back
 """
 import requests
 
-response = requests.post('/url/to/query/')
+user_request = {
+    "age": 52,
+    "workclass": "Self-emp-not-inc",
+    "fnlgt": 209642,
+    "education": "HS-grad",
+    "educationNum": 9,
+    "maritalStatus": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capitalGain": 5000,
+    "capitalLoss": 0,
+    "hoursPerWeek": 45,
+    "nativeCountry": "United-States"
+}
 
-print(response.status_code)
-print(response.json())
+resp = requests.get('https://udacity-project3-ashraf.herokuapp.com/')
+
+print(resp.status_code)
+print(resp.json())
